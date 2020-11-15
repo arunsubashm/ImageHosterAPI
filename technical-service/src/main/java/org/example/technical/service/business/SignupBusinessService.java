66d276ced2fd468f6fc96158f3a9d03e8989d221
Arun Subash Manickam <arunsubashm@gmail.com>
2020-11-15 20:25:55 +0530

    Encrypt password

--- technical-service/src/main/java/org/example/technical/service/business/SignupBusinessService.java
+++ technical-service/src/main/java/org/example/technical/service/business/SignupBusinessService.java
@@ -13,10 +13,22 @@ public class SignupBusinessService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private PasswordCryptographyProvider passwordCryptographyProvider;
+
     @Transactional(propagation = Propagation.REQUIRED)
     //This method receives the UserEntity type object and calls createUser() method in UserDao class.
     //This method returns the UserEntity type object which has been stored in a database.
     public UserEntity signup(UserEntity userEntity) {
+
+        String password = userEntity.getPassword();
+        if (password == null){
+            userEntity.setPassword("test");
+        }
+        String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+
         return userDao.createUser(userEntity);
     }
 }
